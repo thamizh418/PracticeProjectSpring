ec34a9834fb6332d9eb0b7987f3fb17783d4ad74
Tamil Kumaran <tamiilkhs@gmail.com>
2024-03-05 19:38:58 +0530

    Initial commit

---
+++ src/main/java/com/example/PractiseProject/model/StudioModel.java
@@ -0,0 +1,23 @@
+package com.example.PractiseProject.model;
+
+public class StudioModel {
+	
+	private String studioName;
+	private String studioId;
+	
+	public String getStudioName() {
+		return studioName;
+	}
+	public void setStudioName(String studioName) {
+		this.studioName = studioName;
+	}
+	public String getStudioId() {
+		return studioId;
+	}
+	public void setStudioId(String studioId) {
+		this.studioId = studioId;
+	}
+	
+	
+
+}
