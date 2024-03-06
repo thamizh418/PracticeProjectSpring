7fffe5c664118015b2a4c95ec73a59c389666866
Tamil Kumaran <tamiilkhs@gmail.com>
2024-03-06 11:26:50 +0530

    2
    Issue fix commit

--- src/main/java/com/example/PractiseProject/dao/StudioDao.java
+++ src/main/java/com/example/PractiseProject/dao/StudioDao.java
@@ -3,7 +3,7 @@ package com.example.PractiseProject.dao;
 import com.example.PractiseProject.model.StudioModel;
 
 public interface StudioDao {
-	
+
 	public StudioModel getStudioDetails(StudioModel studioModel);
 
 }
